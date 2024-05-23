ebee60ce665b9bce2d176d6640be359c53ac5f40
Juan Manuel Espiño <juanm1ta47@gmail.com>
2024-05-23 21:18:51 +0200

    updates

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -107,7 +107,7 @@ export const login = async (req, res) => {
     res.cookie("access_token", token, {
       expires: new Date(Date.now() + 24 * 3600000),
       sameSite: "none",
-      secure: false,
+      secure: true,
       httpOnly: true,
     });
 
@@ -129,7 +129,7 @@ export const logout = async (req, res) => {
     res.clearCookie("access_token", {
       expires: new Date(Date.now() - 1),
       sameSite: "none",
-      secure: false,
+      secure: true,
       httpOnly: true,
     });
 
